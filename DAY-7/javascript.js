e245445649d0c17a1a9463e1bbb98244591fa583
shilpa2529 <shilpakonnoju@gmail.com>
2026-02-17 16:42:29 +0530

    updated javascript project

--- DAY-7/javascript.js
+++ DAY-7/javascript.js
@@ -1,1 +1,20 @@
-const image=document.getElementById("#images");q
+
+let nameelement=document.getElementById("nameelement");
+let passwordelement=document.getElementById("passwordelement");
+let signinelement=document.getElementById("signin")
+
+function signinbutton(){
+  if(nameelement.value=""){
+      alert("please enter username or email");
+  }
+  else if(passwordelement.value=""){
+    alert("please enter password");
+  }
+  else{
+    let anchorelement=document.createElement("a")
+    anchorelement.href="https://education.github.com/git-cheat-sheet-education.pdf";
+    anchorelement.target = "_blank";
+    document.body.appendChild(anchorelement);
+  }
+}
+signinelement.addEventListener("click",signinbutton());
